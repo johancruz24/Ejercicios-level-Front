d6cb1b56a47e15abda2318e930c146294d703b20
johancruz24 <62783655+johancruz24@users.noreply.github.com>
2020-10-26 14:00:44 -0500

    Add array Peoples

--- src/app/services/people.service.ts
+++ src/app/services/people.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { People } from '../models/people';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +9,11 @@ export class PeopleService {
 
   URL_API = 'http://localhost:4000/api/peoples'
 
-    
+  peoples  : People[];  
   constructor(private http: HttpClient) { }
 
   getPeople(){
-    return this.http.get(this.URL_API);
+    return this.http.get<People[]>(this.URL_API);
     
   }
 }
