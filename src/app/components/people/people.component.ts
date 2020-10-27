9fe98122c60cb7dfebc833069023c45d22ffd4d5
johancruz24 <62783655+johancruz24@users.noreply.github.com>
2020-10-26 22:09:51 -0500

    Create add and delete function

--- src/app/components/people/people.component.ts
+++ src/app/components/people/people.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {PeopleService} from '../../services/people.service'
+import {PeopleService} from '../../services/people.service';
+import {NgForm} from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-people',
@@ -23,8 +26,63 @@ export class PeopleComponent implements OnInit {
     )
   }
 
-  addPeople(form){
+  
+  addPeople(form: NgForm){
+    console.log(form.value.mail);
+    
+    
+    
+    var emailCompare =  /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    var nameCompare =  /^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$/;
 
+    var restultName = nameCompare.test(form.value.name);
+    var restultMail = emailCompare.test(form.value.mail);
+
+    console.log(restultName);
+    console.log(restultMail);
+    
+
+    if(form.value.km <= 4 && form.value.km != null && form.value.km != 0){
+      return alert("Let's Go, You must to walk more")
+    }else if(form.value.km == null){
+      return alert("The distance traveled must not have a null value")
+    }else if(form.value.km == 0){
+      return alert("The distance traveled must be greater than zero")
+    }else if(form.value.name == ('')){
+      return alert("Enter a value for the name field")
+    }
+    if(restultName != true){
+      return alert("Please Enter a value for the name like Pepito Perez")
+    }else if(form.value.mail == ('')){
+      return alert("Enter a value for the Email field")
+    }else if(restultMail != true){
+      return alert("Please Enter a value for the Email like pepitoperez@xxxxxx.com")
+    }else{
+      this.peopleService.createPeople(form.value).subscribe(
+        res => {
+          this.getPeoples();
+          form.reset();
+          alert("Congratulations !!!")
+        },
+        err => console.error(err)
+        
+      )    
+
+    }
+
+    
+  }
+
+  deletePeople(id:string){
+    if(confirm('Do you want to remove the record?')){
+      this.peopleService.deletePeople(id)
+      .subscribe(
+        res => {
+          this.getPeoples();
+        },
+        err => console.error(err)
+      )
+    }
   }
 
 }
