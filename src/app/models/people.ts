06e16a806e2443ab4788c3486b54b64b76d023b5
johancruz24 <62783655+johancruz24@users.noreply.github.com>
2020-10-26 22:19:38 -0500

    Edit interface People value required

--- src/app/models/people.ts
+++ src/app/models/people.ts
@@ -2,7 +2,7 @@ export interface People {
     name: string
     mail: string
     km: number
-    createdAt: string
-    updatedAt: string
-    _id: string
+    createdAt?: string
+    updatedAt?: string
+    _id?: string
 }
