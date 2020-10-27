3d819d3c11b009d5daf8160ff6c35571e23a1f25
johancruz24 <62783655+johancruz24@users.noreply.github.com>
2020-10-26 22:21:12 -0500

    Context Selected People

--- src/app/services/people.service.ts
+++ src/app/services/people.service.ts
@@ -1,19 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { People } from '../models/people';
+import { isNull } from '@angular/compiler/src/output/output_ast';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeopleService {
-
+ 
   URL_API = 'http://localhost:4000/api/peoples'
 
+  selectedPeople: People={
+    name: '',
+    mail: '',
+    km: null,
+  }; 
   peoples  : People[];  
   constructor(private http: HttpClient) { }
 
   getPeople(){
-    return this.http.get<People[]>(this.URL_API);
-    
+    return this.http.get<People[]>(this.URL_API);    
+  }
+
+  createPeople(people: People){
+    return this.http.post(this.URL_API, people)
+
+  }
+
+  deletePeople(_id: String){
+    return this.http.delete(`${this.URL_API}/${_id}`)   
   }
 }
